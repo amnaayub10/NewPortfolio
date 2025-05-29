727877581b4166c67427c9439e85397002cd2100
amnaayub10 <138980787+amnaayub10@users.noreply.github.com>
2025-05-29 18:19:32 +0500

    First commit

---
+++ src/content_option.js
@@ -0,0 +1,181 @@
+const logotext = "Amna Ayub";
+const meta = {
+    title: "Amna Ayub",
+    description: "I’m Amna Ayub, a full stack developer with a passion for creating beautiful and functional web applications.",
+    
+};
+
+const introdata = {
+    title: "I’m Amna Ayub",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "Bringing ideas to life with React, Node, TypeScript & Tailwind finesse.",
+    },
+    description: "I’m a full stack developer with a passion for creating beautiful and functional web applications. I have experience in both front-end and back-end development, and I love working with the latest technologies to build innovative solutions.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "As bit about me",
+    aboutme: "I’m a full-stack web developer specializing in building responsive, scalable, and user-focused applications using React, Node.js, TypeScript, and Tailwind CSS. I combine clean code with modern design principles to deliver seamless frontend experiences and robust backend logic. Whether it's crafting sleek user interfaces or integrating powerful APIs, I focus on performance, maintainability, and great developer experience. I'm passionate about solving real-world problems through code and constantly learning new tools to stay ahead in the evolving tech landscape.",
+};
+const worktimeline = [{
+        jobtitle: "Web Developwer",
+        where: "Zee Tech",
+        date: "2024-2025",
+    },
+    {
+        jobtitle: "Scocail Media Manager",
+        where: "The Educators",
+        date: "2020-2023",
+    },
+    {
+        jobtitle: "Web Developer",
+        where: "The Capital Technologies",
+        date: "2018",
+    },
+     {
+        jobtitle: "Socail Media Manager",
+        where: "Youth Times",
+        date: "2015-2020",
+    },
+];
+
+const skills = [{
+        name: "ReactJs",
+        
+    },
+    {
+        name: "NodeJs",
+        
+    },
+    {
+        name: "ExpressJs",
+       
+    },
+    {
+        name: "Javascript",
+    
+    },
+    {
+        name: "Tailwind CSS",
+       
+    },
+    {
+        name: "Next JS",
+        
+    },
+];
+
+const services = [{
+        title: "Frontend Development",
+        description: "Frontend developer specializing in modern JavaScript frameworks like React and TypeScript.I create responsive, accessible, and visually engaging interfaces using Tailwind CSS and best UX practices.Focused on performance, scalability, and delivering pixel-perfect web experiences across all devices.",
+    },
+    {
+        title: "Backend Development",
+        description: "Backend developer with expertise in Node.js and Express, building robust APIs and server-side applications. I ensure data integrity, security, and performance through efficient database design and RESTful architecture. Passionate about creating scalable solutions that power modern web applications.",
+       
+    },
+    {
+        title: "Social Media Management",
+        description: "Social media manager with a knack for crafting engaging content and building brand presence across platforms. I develop data-driven strategies to enhance audience engagement, drive traffic, and boost conversions. Proficient in analytics tools to measure success and optimize campaigns.",
+        
+    },
+];
+
+const dataportfolio = [{
+        img: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/itUG4rTJjWyU/v0/-1x-1.webp",
+        description: "Solar Panels Installation and Maintenance",
+        link: "https://ecocleansolarsolutionz.web.app/",
+         target: "_blank",
+    },
+    {
+        img: "https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Caputure Life in Perfect Focus .",
+        link: "https://aa-tech-1.web.app/",
+         target: "_blank",
+    },
+    {
+        img: "https://images.pexels.com/photos/587741/pexels-photo-587741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Create. Connect. Captivate. Celebrate.",
+        link: "https://shadcn-ochre.vercel.app/",
+            target: "_blank",
+    },
+   /* {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },*/
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "amna.bsse2792@iiu.edu.pk",
+    YOUR_FONE: "+923045679215",
+    description: "I’m a full stack developer with a passion for creating beautiful and functional web applications. I have experience in both front-end and back-end development, and I love working with the latest technologies to build innovative solutions.",
+   
+    YOUR_SERVICE_ID: "service_gk8811e",
+    YOUR_TEMPLATE_ID: "template_c6easzj",
+    YOUR_USER_ID: "0SDMQRih-GE6nlygP",
+};
+
+const socialprofils = {
+    github: "https://github.com/amnaayub10",
+    facebook: "https://www.facebook.com/profile.php?id=100005448888735&mibextid=ZbWKwL",
+    linkedin: "https://www.linkedin.com/in/amna-ayub-29087124a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/ayub.amna14?igsh=MXh1ZXh1M3ozdTZoYg==",
+   
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
